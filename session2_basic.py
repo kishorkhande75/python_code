003cd8472706f899ddddf5f51bf302fe4621d59e
Kishor Khande <kishorkhande75@gmail.com>
2022-12-06 12:30:14 +0530

    first commint

---
+++ session2_basic.py
@@ -0,0 +1,22 @@
+# first_num = 10
+# second_num = 20
+# addition = first_num + second_num
+# print(addition)
+
+# ## variable name should start with lower case charactor
+# ## digits are allowed but not at the start
+# ## _ allowed (only underscore is allowed in starting)
+
+# # function
+# #  1. id
+# # 2. type
+
+# print(type(first_num), type(second_num))
+# print(id(first_num), id(second_num))
+
+a = 10
+b = 10
+
+print(id(a), id (b))
+
+## When you entering same value to the variable then memory location is the same
---
+++ session3.py
@@ -0,0 +1,58 @@
+# Datatypes:
+# 1 Int :
+# num1 = 100
+# print(type(num1))
+
+# when you want to store the int to variable
+# --------------------------------------------------------
+# 2 Float
+
+# num2 = 15.25
+# print(type(num2))
+
+
+## Description:-"int" and "float" is inmuteble type (when you entering the same variable with differnt value then the memory loacation are differnt)
+# Example :
+# num1 = 100
+# print(id(num1))
+# num2 = 105
+# print(id(num2))
+# -------------------------------------------
+
+## 3 str '' "" """ """
+
+# s = "Python"
+# print(id(s))
+# print(s,type(s))
+
+# s = "new value"
+# print(id(s))
+## ----------------------------------------------------------------
+## 4 List [] / Description:- List is the mutable. we can add or delete in the same memory location
+
+# l = [10,20,30,40,50,"Python", "Django"]
+# print(l)
+# print(id(l))
+# l.append(60)
+# print(id(l))
+# print(l)
+
+## --------------------------------------------------------------------------
+
+## 5 tuple ():- Tuple is the unmutable because we can't change, delete and update in tuple.
+
+# t = (10, 20, 30)
+
+## --------------------------------------------------------------------------
+## 6 Dict {} /- List is the mutable. we can add or delete in the same memory location
+# d = {"name":"ACB","email":"abc@gmail.com"}
+
+## ---------------------------------------------------------------------------------
+## 7 Set {} :- List is the mutable. we can add or delete in the same memory location
+# s = {10,20,30,40}
+
+## ---------------------------------------------------------------------------------
+## 8 bool : True and False
+
+## ---------------------------------------------------------------------------------
+## complex : 4 + 3j
